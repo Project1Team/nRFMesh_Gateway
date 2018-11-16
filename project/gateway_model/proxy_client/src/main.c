12618920cc8bf63a31a89a0c2005c3df09b4e05d
namnguyenceuit <ewardsnguyen@gmail.com>
2018-11-16 11:04:57 +0700

    [Merged] Temporarily merged sensor example into mesh

--- project/gateway_model/proxy_client/src/main.c
+++ project/gateway_model/proxy_client/src/main.c
@@ -37,6 +37,7 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
 /* HAL */
 #include "boards.h"
@@ -71,12 +72,37 @@
 #include "log.h"
 #include "rtt_input.h"
 
-/* Example specific includes */
+/* Gateway model specific includes */
 #include "app_config.h"
 #include "nrf_mesh_config_thesis.h"
 #include "gateway_model_common.h"
 #include "thesis_common.h"
 #include "nrf_gpio.h"
+
+/* TWI Specific includes */
+#include "app_util_platform.h"
+#include "app_error.h"
+#include "nrf_drv_twi.h"
+#include "nrf_delay.h"
+
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+/* TWI instance ID. */
+#define TWI_INSTANCE_ID     0
+
+/* Common addresses definition for temperature sensor. */
+#define LM75B_ADDR          (0x90U >> 1)
+
+#define LM75B_REG_TEMP      0x00U
+#define LM75B_REG_CONF      0x01U
+#define LM75B_REG_THYST     0x02U
+#define LM75B_REG_TOS       0x03U
+
+/* Mode for LM75B. */
+#define NORMAL_MODE 0U
 
 #define MSG_0                (9)
 #define MSG_1                (27)
@@ -90,7 +116,6 @@
 
 #define LED_PIN              (2)
 
-
 #define APP_UNACK_MSG_REPEAT_COUNT   (2)
 
 #define DEVICE_NAME                     "Mesh Sensor Node"
@@ -101,6 +126,7 @@
 #define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(100)                        /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called. */
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(2000)                       /**< Time between each call to sd_ble_gap_conn_param_update after the first call. */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
+
 
 static void gap_params_init(void);
 static void conn_params_init(void);
@@ -521,3 +547,149 @@ int main(void)
         (void)sd_app_evt_wait();
     }
 }
+
+
+
+/*-------------------------------------------------TWI MAIN FUNCTIONS TEMPORARILY MERGE-----------------------------------------------------*/
+///* Indicates if operation on TWI has ended. */
+//static volatile bool m_xfer_done = false;
+//
+///* TWI instance. */
+//static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+//
+///* Buffer for samples read from temperature sensor. */
+//static uint8_t m_sample;
+//
+///**
+// * @brief Function for setting active mode on LM75.
+// */
+//void LM75B_set_mode(void)
+//{
+//    ret_code_t err_code;
+//
+//    /* Writing to LM75B_REG_CONF "0" set temperature sensor in NORMAL mode. */
+//    uint8_t reg[2] = {LM75B_REG_CONF, NORMAL_MODE};
+//    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, sizeof(reg), false);
+//    APP_ERROR_CHECK(err_code);
+//    while (m_xfer_done == false);
+//
+//    /* Writing to pointer byte. */
+//    reg[0] = LM75B_REG_TEMP;
+//    m_xfer_done = false;
+//    err_code = nrf_drv_twi_tx(&m_twi, LM75B_ADDR, reg, 1, false);
+//    APP_ERROR_CHECK(err_code);
+//    while (m_xfer_done == false);
+//}
+//
+///**
+// * @brief Function for handling data from temperature sensor.
+// *
+// * @param[in] temp          Temperature in Celsius degrees read from sensor.
+// */
+//__STATIC_INLINE void data_handler(uint8_t temp)
+//{
+//    NRF_LOG_INFO("Temperature: %d Celsius degrees.", temp);
+//}
+//
+///**
+// * @brief TWI events handler.
+// */
+//void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
+//{
+//    switch (p_event->type)
+//    {
+//        case NRF_DRV_TWI_EVT_DONE:
+//            if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX)
+//            {
+//                data_handler(m_sample);
+//            }
+//            else if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_TX)
+//                 {
+//                    NRF_LOG_INFO("Test 0.1.");
+//                 //m_xfer_done = true;
+//                 }
+//                 else if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_TXRX)
+//                      {
+//                          NRF_LOG_INFO("Test 0.2.");
+//                      } 
+//                      else
+//                      {
+//                          NRF_LOG_INFO("Test 0.3.");
+//                      }
+//                        m_xfer_done = true;
+//
+//            break;
+//        case NRF_DRV_TWI_EVT_ADDRESS_NACK:
+//            NRF_LOG_INFO("Test 1.");
+//            break;
+//        case NRF_DRV_TWI_EVT_DATA_NACK:
+//            NRF_LOG_INFO("Test 2.");
+//            break;
+//        default:
+//            break;
+//    }
+//}
+//
+///**
+// * @brief UART initialization.
+// */
+//void twi_init (void)
+//{
+//    ret_code_t err_code;
+//
+//    const nrf_drv_twi_config_t twi_lm75b_config = {
+//       .scl                = ARDUINO_SCL_PIN,
+//       .sda                = ARDUINO_SDA_PIN,
+//       .frequency          = NRF_DRV_TWI_FREQ_100K,
+//       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+//       .clear_bus_init     = false
+//    };
+//
+//    err_code = nrf_drv_twi_init(&m_twi, &twi_lm75b_config, twi_handler, NULL);
+//    APP_ERROR_CHECK(err_code);
+//
+//    nrf_drv_twi_enable(&m_twi);
+//}
+//
+///**
+// * @brief Function for reading data from temperature sensor.
+// */
+//static void read_sensor_data()
+//{
+//    m_xfer_done = false;
+//
+//    /* Read 1 byte from the specified address - skip 3 bits dedicated for fractional part of temperature. */
+//    ret_code_t err_code = nrf_drv_twi_rx(&m_twi, LM75B_ADDR, &m_sample, sizeof(m_sample));
+//    APP_ERROR_CHECK(err_code);
+//}
+//
+//
+//int main(void)
+//{
+//    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+//    NRF_LOG_DEFAULT_BACKENDS_INIT();
+//
+//    NRF_LOG_INFO("\r\nTWI sensor example started.");
+//    NRF_LOG_FLUSH();
+//    twi_init();
+//    LM75B_set_mode();
+//
+//    //for (uint8_t i = 0; i < 20; i++)
+//    while (true)
+//    {
+//        //m_xfer_done = false;
+//        nrf_delay_ms(1000);
+//
+//
+//
+//        read_sensor_data();
+//        
+//                do
+//        {
+//            __WFE();
+//        }while (m_xfer_done == false);
+//        NRF_LOG_FLUSH();
+//    }
+//
+//}
+
